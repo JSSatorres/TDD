84f21c8e6a07b9299029ec7c01f659d5377a9959
jssatorres <juansataz.dev@gmail.com>
2022-11-09 08:47:42 +0100

    fix (fizzbuzz) refactor

--- firstTDD/src/fizzbuzz.js
+++ firstTDD/src/fizzbuzz.js
@@ -2,11 +2,19 @@ const fizzbuzz = (number) => {
   if (typeof number !== 'number') throw new Error('parameter must be a number')
   if (Number.isNaN(number)) throw new Error('parameter must be a number')
 
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
-  if (number % 3 === 0) return 'fizz'
-  if (number % 5 === 0) return 'buzz'
+  const multiplies = { 3: 'fizz', 5: 'buzz' }
+  let output = ''
 
-  return number
+  Object.entries(multiplies).forEach(([multiplier, word]) => {
+    if (number % multiplier === 0) output += word
+  })
+
+  return output === '' ? number : output
 }
 
 export default fizzbuzz
+
+// before refactor
+// if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz'
+// if (number % 3 === 0) return 'fizz'
+// if (number % 5 === 0) return 'buzz
